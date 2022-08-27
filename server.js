fb81588562e86b0c5d181d4c44bf699b4fb8f42e
Cory Metcalf <coco@Corys-MacBook-Pro.local>
2022-08-26 22:29:53 -0600

    added more to server.js

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const path = require('path');
 const api = require('./Develop/public/assets/index.js');
+const fs = require('fs');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+  });
+
+app.get('/api/', (req, res) => res.json(petData));
+
+app.listen(PORT, () => {
+    console.log(`App listening at http://localhost:${PORT}`);
+  });
