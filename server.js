ab886884cf307505f79ecd9e4e97c385fe9e8f0b
Cory Metcalf <coco@Corys-MacBook-Pro.local>
2022-08-29 15:53:04 -0600

    added api route

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const api = require('./Develop/public/assets/index.js');
 const fs = require('fs');
+const database = require('mime-db');
 
 const PORT = process.env.PORT || 3001;
 
@@ -13,10 +14,38 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, '/Develop/public/index.html'));
   });
 
-app.get('/api/', (req, res) => res.json(petData));
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, '/Develop/public/notes.html'));
+});
+
+app.route("/api/notes")
+    .get(function (req, res) {
+        let jsonFilePath = path.join(__dirname, "/db/db.json");
+        let newNote = req.body;
+
+        let highestId = 99;
+        for (let i = 0; i < database.length; i++) {
+            let individualNote = database[i];
+            if(individualNote.id > highestId) {
+                highestId = individualNote.id;
+            }
+        }
+
+        newNote.id = highestId + 1;
+        database.push(newNote)
+        fs.writeFile(jsonFilePath, JSON.stringify(database), function (err) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log("Your note was saved.");
+        });
+        res.json(newNote);
+    });
+
+
 
 app.listen(PORT, () => {
     console.log(`App listening at http://localhost:${PORT}`);
